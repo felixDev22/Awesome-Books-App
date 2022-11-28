a85599a6e05dbbb9233a1919352ba075ed8b61b8
shegy28 <shegy28@gmail.com>
2022-11-28 17:34:11 +0200

    Add book list function

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-const book = document.querySelector('.books-container');
-const bookTitle = document.querySelector('.title');
-const author = document.querySelector('#author');
+const bookContainer = document.querySelector('.books-container');
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
 const addBtn = document.querySelector('.add-btn');
 
-let booklist = [
-  {
-    title: '',
-    author: '',
-  },
-];
+let bookList = [];
 
-if (localStorage.getItem('books')) {
-  booksData = JSON.parse(localStorage.getItem('books'));
+function bookUpload() {
+  bookContainer.innerHTML = '';
+  for (let i = 0; i < bookList.length; i++) {
+    bookContainer.innerHTML += `
+    <div class="books">
+    <h3>${bookList[i].bookTitle}</h3>
+    <h3>${bookList[i].bookAuthor}</h3>
+    <button onclick="deleted(${i})">Remove</button>
+    <hr />
+  </div>
+   `;
+    bookTitle.value = '';
+    bookAuthor.value = '';
+  }
 }
+
+function deleted(index) {
+  bookList.splice(index, 1);
+  bookUpload();
+  localStorage.setItem('bookList', JSON.stringify(bookList));
+}
+
+window.onload = () => {
+  if (localStorage.getItem('bookList')) {
+    bookList = JSON.parse(localStorage.getItem('bookList'));
+  }
+  bookUpload();
+};
+
+addBtn.addEventListener('click', () => {
+  const books = {
+    bookTitle: bookTitle.value,
+    bookAuthor: bookAuthor.value,
+  };
+  bookList.push(books);
+  bookUpload();
+  localStorage.setItem('bookList', JSON.stringify(bookList));
+});
