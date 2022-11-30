0061337c345e14da40abb3acf26ecca3586baaac
shegy28 <shegy28@gmail.com>
2022-11-30 17:09:25 +0200

    Add single page navigation feature

--- script.js
+++ script.js
@@ -90,16 +90,16 @@ newDate.innerHTML = currentDate;
 // add event listners
 function displayList() {
   document.getElementById('Book-list').classList.add('block');
-  document.getElementById('Add-new').classList.add('hide');
-  document.getElementById('contact').classList.add('hide');
+  document.getElementById('Add-new').classList.remove('block');
+  document.getElementById('contact').classList.remove('block');
 }
 function displayNew() {
   document.getElementById('Add-new').classList.add('block');
-  document.getElementById('Book-list').classList.add('hide');
-  document.getElementById('contact').classList.add('hide');
+  document.getElementById('Book-list').classList.remove('block');
+  document.getElementById('contact').classList.remove('block');
 }
 function displayContact() {
   document.getElementById('contact').classList.add('block');
-  document.getElementById('Book-list').classList.add('hide');
-  document.getElementById('Add-new').classList.add('hide');
+  document.getElementById('Book-list').classList.remove('block');
+  document.getElementById('Add-new').classList.remove('block');
 }
